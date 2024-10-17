96a492c90cea1789028149dc6cf4100d2b9be545
Mr_Smile_000 <2469724709@qq.com>
2024-10-17 12:18:54 +0800

    smile

---
+++ vue-admin/src/components/Home/news.vue
@@ -0,0 +1,81 @@
+<template>
+	<div class="news-card">
+		<title_ Chinese="新闻" English="NEWS"></title_>
+
+		<div v-for="newsItem in news" :key="newsItem.id">
+			<div class="news-item">
+				<div class="title" style="margin-bottom: 1.5%">
+					{{ newsItem.title }}
+				</div>
+				<div
+					class="content"
+					style="
+						color: #acaba5;
+						overflow: hidden;
+						display: -webkit-box;
+						line-clamp: 3;
+						-webkit-box-orient: vertical;
+						-webkit-line-clamp: 3;
+						margin-bottom: 4%;
+					"
+				>
+					{{ newsItem.content }}
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup>
+import { defineProps, ref } from "vue";
+
+import title_ from "@/components/Home/title.vue";
+
+defineProps({
+	news: {
+		type: Array,
+		default: () => [
+			{
+				id: 1,
+				title: "新闻1",
+				content:
+					"海明威的一生之中曾荣获不少奖项。他在第一次世界大战期间被授予银制勇敢勋章；1953年，他以《老人与海》一书获得普利策奖 [1]；1954年的《老人与海》又为海明威夺得诺贝尔文学奖。2001年，海明威的《太阳照常升起》 [9]（The Sun Also Rises）与《永别了，武器》两部作品被美国现代图书馆列入“20世纪中的100部最佳英文小说”中。",
+			},
+			{
+				id: 2,
+				title: "新闻2",
+				content:
+					"海明威的一生之中曾荣获不少奖项。他在第一次世界大战期间被授予银制勇敢勋章；1953年，他以《老人与海》一书获得普利策奖 [1]；1954年的《老人与海》又为海明威夺得诺贝尔文学奖。2001年，海明威的《太阳照常升起》 [9]（The Sun Also Rises）与《永别了，武器》两部作品被美国现代图书馆列入“20世纪中的100部最佳英文小说”中。",
+			},
+			{
+				id: 3,
+				title: "新闻3",
+				content:
+					"海明威的一生之中曾荣获不少奖项。他在第一次世界大战期间被授予银制勇敢勋章；1953年，他以《老人与海》一书获得普利策奖 [1]；1954年的《老人与海》又为海明威夺得诺贝尔文学奖。2001年，海明威的《太阳照常升起》 [9]（The Sun Also Rises）与《永别了，武器》两部作品被美国现代图书馆列入“20世纪中的100部最佳英文小说”中。",
+			},
+		],
+	},
+});
+
+const activeName = ref(1);
+</script>
+
+<style scoped>
+.news-card {
+	height: 40vh;
+}
+
+.news-item {
+	margin-bottom: 10px;
+}
+
+.content {
+	color: #acaba5;
+	overflow: hidden;
+	display: -webkit-box;
+	line-clamp: 3;
+	-webkit-box-orient: vertical;
+	-webkit-line-clamp: 3;
+	margin-bottom: 5%;
+}
+</style>
---
+++ vue-user/src/router/index.js
@@ -0,0 +1,72 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+	{
+		path: "/",
+		component: () => import("@/views/Manager.vue"),
+		redirect: "/home",
+		children: [
+			// 首页
+			{
+				path: "home",
+				name: "home",
+				params: {
+					showText: "首页",
+					role: null,
+					icon: "HomeFilled",
+					show: true,
+				},
+				component: () => import("@/views/manager/Home.vue"),
+			},
+			// 购物页
+			{
+				path: "buy",
+				name: "buy",
+				params: {
+					showText: "文创商城",
+					role: "USER",
+					icon: "Goods",
+					show: true,
+				},
+				component: () => import("@/views/manager/Buy.vue"),
+			},
+			// 订单管理
+			{
+				path: "orders",
+				name: "orders",
+				params: { showText: "订单管理", icon: "Tickets", show: true },
+				component: () => import("@/views/manager/Orders.vue"),
+			},
+			// 个人资料
+			{
+				path: "person",
+				name: "person",
+				params: { showText: "个人资料", icon: "User", show: true },
+				component: () => import("@/views/manager/Person.vue"),
+			},
+			// 修改密码
+			{
+				path: "password",
+				name: "password",
+				params: { showText: "修改密码", icon: "Lock", show: true },
+				component: () => import("@/views/manager/Password.vue"),
+			},
+			// 文章模版页
+			{
+				path: "article", // 使用动态参数 myData
+				name: "article",
+				component: () => import("@/views/manager/Article.vue"),
+				params: { show: false }, // 导航栏不显示
+			},
+		],
+	},
+	{ path: "/login", component: () => import("@/views/Login.vue") },
+	{ path: "/register", component: () => import("@/views/Register.vue") },
+];
+
+const router = createRouter({
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes,
+});
+
+export default router;
